1d46958231d304d28265a4e279bac970a1a28eca
nyakoa <126573126+nyakoa@users.noreply.github.com>
2025-02-18 17:56:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// const buttons = document.querySelectorAll(".btn");
+// buttons.forEach((button) => {
+//   button.addEventListener("click", function () {
+//     button.style.transform = "scale(1.2";
+//   });
+// });
